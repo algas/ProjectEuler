a13d4c9a9556cf278389d04a7552feec6ec7ff04
algas <sgt.yamauchi@gmail.com>
2012-07-10 09:55:57 +0900

    Problem 4

---
+++ haskell/P4.hs
@@ -0,0 +1,10 @@
+isPalindromic :: Int -> Bool
+isPalindromic x = (show x) == (reverse (show x))
+
+threeDigits = reverse [100..999]
+
+p4 :: Int
+p4 = maximum [x * y | x <- threeDigits, y <- threeDigits, isPalindromic (x * y)]
+
+main = print $ p4
+
