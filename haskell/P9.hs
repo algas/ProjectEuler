4727ba69839f9531b917556ffb6159dabd6257a5
algas <sgt.yamauchi@gmail.com>
2012-07-10 21:27:44 +0900

    Problem 9

---
+++ haskell/P9.hs
@@ -0,0 +1,8 @@
+c :: Int -> Int -> Int
+c a b = 1000 - (a + b)
+
+p9 = head [a*b*(n-(a+b)) | a <- [1..n], b <- [1..n], (a*a + b*b) == (c a b)*(c a b)]
+    where
+        n = 1000
+
+main = print $ p9
