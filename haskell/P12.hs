589f11bca713e49a5286e0a9e680f6ed77a34d35
algas <sgt.yamauchi@gmail.com>
2012-07-16 16:55:32 +0900

    Problem 12

---
+++ haskell/P12.hs
@@ -0,0 +1,18 @@
+import Data.List
+
+triangleNumber :: Int -> Int
+triangleNumber 1 = 1
+triangleNumber n = n + triangleNumber (n - 1)
+
+sqrtInt :: Int -> Int
+sqrtInt = floor . sqrt . fromIntegral
+
+flatten :: [[a]] -> [a]
+flatten = foldl (++) []
+
+factors :: Int -> [Int]
+factors x = nub $ sort $ flatten [ [n, (x `div` n)] | n <- [1..(sqrtInt x)], x `mod` n == 0]
+
+p12 = triangleNumber (1 + length (takeWhile (<501) [length (factors (triangleNumber n)) | n <- [1..]]))
+
+main = print p12
