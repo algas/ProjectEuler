6a609652595acf42d02a74b3dcd81d6f30a20245
algas <sgt.yamauchi@gmail.com>
2013-03-04 16:22:59 +0900

    Refactor Scala P1

--- scala/projecteuler/src/main/scala/P1.scala
+++ scala/projecteuler/src/main/scala/P1.scala
@@ -2,7 +2,6 @@ package com.example.projecteuler
 
 class P1 {
   def solve() : Int = {
-    val lst = for (i <- 1 to 999 if (i % 3 == 0 || i % 5 == 0)) yield i
-    lst.sum
+    (1 to 999).toList.filter(p => (p % 3 == 0 || p % 5 == 0)).sum
   }
 }
